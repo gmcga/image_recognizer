bb8b834b342ba919b24316ee9ed3d5ab313a2840
kyleosung <kyleosung@gmail.com>
2023-08-19 14:44:03 -0400

    small edits

--- gui.py
+++ gui.py
@@ -11,10 +11,13 @@ class ImageRec:
         x, y = event.x, event.y
         self.canvas.create_oval(x, y, x+10, y+10, fill='black')
 
-root = tk.Tk()
 
-app = ImageRec(root)
+def main():
 
-root.mainloop()
+    root = tk.Tk()
+
+    app = ImageRec(root)
+
+    root.mainloop()
 
 
